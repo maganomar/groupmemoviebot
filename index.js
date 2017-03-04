ccc26bb103036f90d6a3d192b7a8f2ba3723d219
Magan Omar <maganomar@Magans-MacBook-Pro.local>
2017-03-04 01:17:08 -0500

    next iteration

--- index.js
+++ index.js
@@ -1,36 +1,127 @@
-var http, director, cool, bot, router, server, port;
+var HTTPS = require('https');
+// var cool = require('cool-ascii-faces');
+var http        = require('http');
+var botID = process.env.BOT_ID;
 
-var test;
-http        = require('http');
-director    = require('director');
-cool        = require('cool-ascii-faces');
-bot         = require('./bot.js');
+var testconsole = "";
+var test = "";
+var finalresponse = "old response";
+//------------------
 
-router = new director.http.Router({
-  '/' : {
-    post: bot.respond,
-    get: ping
-  }
-});
 
-server = http.createServer(function (req, res) {
-  req.chunks = [];
-  req.on('data', function (chunk) {
-    req.chunks.push(chunk.toString());
+// movie url example:
+// http://www.omdbapi.com/?t=Batman+Begins&y=&plot=short&r=json&tomatoes=true
+
+  var options = {
+  host: 'www.omdbapi.com',
+  path: '/?t=Batman+Begins&y=&plot=short&r=json&tomatoes=true',
+  method: 'GET'
+  };
+
+var callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
   });
 
-  router.dispatch(req, res, function(err) {
-    res.writeHead(err.status, {"Content-Type": "text/plain"});
-    res.end(err.message);
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    
+    test = str;
+    var obj = JSON.parse(str);
+    //console.log(obj.Title);
+    var tomatoRating = obj.tomatoMeter;
+    var imdbRating = obj.imdbRating;
+    var movieTitle = obj.Title
+    var metacriticRating = obj.Metascore;
+    finalresponse = movieTitle + "'s rating on Rotten Tomatoes is " + tomatoRating;
+    console.log(test);
+    console.log(testconsole);
+    //return finalresponse;
+    console.log(finalresponse);
+
   });
-});
-
-port = Number(process.env.PORT || 5000);
-server.listen(port);
-
-
-function ping() {
-  this.res.writeHead(200);
-  this.res.end(test);
 }
+http.request(options, callback).end();
+function movieResponse(){
+
+
+
+
+return finalresponse;
+
+
+}
+
+//-----------
+
+
+function respond() {
+  var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /^\/cool guy$/;
+      
+      //botRegex = thedarkknight;
+
+//botRegex.test(request.text)) {
+  //if (request.text && request.text == "Movie Bot"){
+
+  if(request.text && botRegex.test(request.text)){ 
+
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+}
+
+
+
+
+function postMessage() {
+  var botResponse, options, body, botReq;
+
+  // test
+  //http.request(options, callback).end();
+  botResponse = movieResponse();
+
+
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
+exports.respond = respond;
+
 
